ce84091825aa41aa0099408dc46795a8f43f2ca2
bjornregnell <bjorn.regnell@cs.lth.se>
2017-02-08 21:55:17 +0100

    reformat code

--- src/main/scala/simpleparts/Par.scala
+++ src/main/scala/simpleparts/Par.scala
@@ -4,19 +4,21 @@ object Par {
 
   import scala.util.Try
 
-  def spawn(codeBlock: => Unit) = {
+  def spawn(codeBlock: => Unit): Thread = {
     val t = new Thread(new Runnable { def run = codeBlock })
     t.start
     t
   }
 
-  def spawnLoop(codeBlock: => Unit) = spawn { while (true) codeBlock }
+  def spawnLoop(codeBlock: => Unit): Thread = spawn { while (true) codeBlock }
 
-  def spawnLoopRecover(codeBlock: => Unit)(onError: Throwable => Unit) = spawn {
-    var continue = true
-    while (continue) Try {codeBlock} recover { case e =>
-      continue = false
-      onError(e)
+  def spawnLoopRecover(codeBlock: => Unit)(onError: Throwable => Unit): Thread =
+    spawn {
+      var continue = true
+      while (continue) Try { codeBlock } recover {
+        case e =>
+          continue = false
+          onError(e)
+      }
     }
-  }
 }
