f603cb9341a8d43b5fd34977b4954877560fc339
bjornregnell <bjorn.regnell@cs.lth.se>
2017-02-14 22:27:58 +0100

    update temrinal with jline fix to avoid space in compl

--- src/main/scala/simpleparts/Terminal.scala
+++ src/main/scala/simpleparts/Terminal.scala
@@ -2,12 +2,16 @@ package simpleparts
 
 object Terminal {
   val reader = new jline.console.ConsoleReader
+  val completionHdlr = new jline.console.completer.CandidateListCompletionHandler
+  completionHdlr.setPrintSpaceAfterFullCompletion(false)
+  reader.setCompletionHandler(completionHdlr)
 
   final val CtrlD = "\u0004"  // End Of Transmission
 
   private def replaceNull(s: String): String = if (s == null) CtrlD else s
 
-  def get(prompt: String = ""): String = replaceNull(reader.readLine(prompt))
+  def get(prompt: String = "", default: String = ""): String =
+    replaceNull(reader.readLine(prompt, null, default))
 
   def getSecret(prompt: String = "Enter secret: "): String =
     replaceNull(reader.readLine(prompt,'*'))
